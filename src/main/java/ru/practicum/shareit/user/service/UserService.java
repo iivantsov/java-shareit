b379a4084f0cb346ff481fc10dd6005836d4c884
iivantsov <iiv.88@yandex.ru>
2024-10-18 19:27:04 +0300

    ShareItApp Sprint-14 /user endpoints implemented

---
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.service;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+public interface UserService {
+
+    User createUser(UserDto userDto);
+
+    User getUser(Long userId);
+
+    User updateUser(Long userId, UserDto userDto);
+
+    void deleteUser(Long userId);
+}
