d5d3df0bb4a9d4b5a966b890e3bee537d8fc748f
iivantsov <iiv.88@yandex.ru>
2024-10-20 00:46:30 +0300

    ShareItApp Sprint-14 Review-1 Added Exception and MethodArgumentNotValidException handling

--- src/main/java/ru/practicum/shareit/exception/AppExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/AppExceptionHandler.java
@@ -2,13 +2,31 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @Slf4j
 @RestControllerAdvice
 public class AppExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse exceptionHandler(Exception exception) {
+        return new ExceptionResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ValidationExceptionResponse> requestBodyExceptionHandler(MethodArgumentNotValidException exception) {
+        List<ValidationExceptionResponse> response = exception.getBindingResult().getFieldErrors().stream()
+                .map(er -> new ValidationExceptionResponse(er.getObjectName(), er.getField(), er.getDefaultMessage()))
+                .toList();
+        log.error("request body validation exception - {}", response);
+        return response;
+    }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
