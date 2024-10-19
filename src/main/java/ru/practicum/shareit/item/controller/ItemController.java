979f2d5a35ba502a96534cc70fb54448651fa38b
iivantsov <iiv.88@yandex.ru>
2024-10-20 01:00:15 +0300

    ShareItApp Sprint-14 Review-1 Added RequestHttpHeaders utility class

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -19,29 +19,29 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    Item addItem(@RequestHeader(value = "X-Sharer-User-Id") long userId, @RequestBody @Valid ItemDto itemDto) {
+    Item addItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) long userId, @RequestBody @Valid ItemDto itemDto) {
         return itemService.addItem(userId, itemDto);
     }
 
     @PatchMapping("/{item-id}")
-    Item updateItem(@RequestHeader(value = "X-Sharer-User-Id") long userId,
+    Item updateItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) long userId,
                     @PathVariable("item-id") long itemId,
                     @RequestBody ItemDto itemDto) {
         return itemService.updateItem(userId, itemId, itemDto);
     }
 
     @GetMapping("/{item-id}")
-    Item getItem(@RequestHeader(value = "X-Sharer-User-Id") long userId, @PathVariable("item-id") long itemId) {
+    Item getItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) long userId, @PathVariable("item-id") long itemId) {
         return itemService.getItem(userId, itemId);
     }
 
     @GetMapping
-    Collection<Item> getAllItems(@RequestHeader(value = "X-Sharer-User-Id") long userId) {
+    Collection<Item> getAllItems(@RequestHeader(value = RequestHttpHeaders.USER_ID) long userId) {
         return itemService.getAllItems(userId);
     }
 
     @GetMapping("/search")
-    Collection<Item> searchItems(@RequestHeader(value = "X-Sharer-User-Id") long userId,
+    Collection<Item> searchItems(@RequestHeader(value = RequestHttpHeaders.USER_ID) long userId,
                                  @RequestParam String text) {
         return itemService.searchItems(userId, text);
     }
