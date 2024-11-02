6b44ea3bc3848dd8cfaae39dcfa0b34a440dc588
iivantsov <iiv.88@yandex.ru>
2024-11-02 15:29:04 +0300

    ShareItApp Sprint-15 User repository reworked to JPA

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -19,25 +19,27 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponseDto createUser(UserRequestDto userRequestDto) {
-        User mappedUser = userMapper.map(userRequestDto);
-        User user = userRepository.createUser(mappedUser);
-        return userMapper.map(user);
+        User user = userMapper.map(userRequestDto);
+        User savedUser = userRepository.save(user);
+        return userMapper.map(savedUser);
     }
 
     @Override
     public UserResponseDto getUser(Long userId) {
-        User user = userRepository.getUser(userId).orElseThrow(() -> new NotFoundException(User.class, userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(User.class, userId));
         return userMapper.map(user);
     }
 
     @Override
     public UserResponseDto updateUser(Long userId, UserRequestDto userRequestDto) {
-        User user = userRepository.updateUser(userId, userRequestDto);
-        return userMapper.map(user);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(User.class, userId));
+        userMapper.updateUserFromDto(user, userRequestDto);
+        User savedUser = userRepository.save(user);
+        return userMapper.map(savedUser);
     }
 
     @Override
     public void deleteUser(Long userId) {
-        userRepository.deleteUser(userId);
+        userRepository.deleteById(userId);
     }
 }
